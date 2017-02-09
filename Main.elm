23a260353996ca4197a0720774bdaf537d85cce2
kogoia <kogo27@gmail.com>
2017-02-10 00:52:07 +0400

    added todo add feature

--- Main.elm
+++ Main.elm
@@ -2,38 +2,66 @@ module Main exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onClick)
+import Html.Events exposing (..)
+
+type alias TodoItemModel = 
+    {
+        id: Int,
+        title: String
+    }
+
+type alias Model = {
+        uid: Int,
+        inputValue: String,
+        todos: List TodoItemModel
+    }
 
 initialModel = { 
+        uid = 3,
+        inputValue = "",
         todos = [
             { id = 1, title = "React" }, 
             { id = 2, title = "Elm" } 
         ] 
     }
 
+type Msg =
+        DeleteTodo Int
+        | AddTodo
+        | UpdateField String
+
 todoItemView todo =
     li []
     [
         div [] [ 
             text todo.title,
-            button [ class "btn", onClick { operation = "DELETE_TODO", data = todo.id }] [text "X"] 
+            button [ class "btn", onClick ( DeleteTodo todo.id ) ] [text "X"] 
         ]
     ]
 
 view model = 
     div [ class "app" ]
     [
+        input [ 
+                value model.inputValue, 
+                onInput UpdateField
+            ] [],
+        button [ onClick AddTodo ] [ text "Add" ],
         ul [ class "results" ] (List.map todoItemView model.todos)
     ]
 
+update: Msg -> Model -> Model
 update msg model =
-    if msg.operation == "DELETE_TODO" then
-        { model | todos = List.filter (\m -> m.id /= msg.data ) model.todos }
-    else
-        model
+    case msg of
+      DeleteTodo id -> 
+            { model | todos = List.filter (\m -> m.id /= id ) model.todos }
+
+      UpdateField str -> { model | inputValue = str }
+
+      AddTodo -> { model | todos = model.todos ++ [ { id = model.uid, title = model.inputValue } ] , uid = model.uid + 1, inputValue = "" }
 
 main = Html.beginnerProgram {
-        view = view,
-        update = update,
-        model = initialModel
-    }
+            view = view,
+            update = update,
+            model = initialModel
+        }
